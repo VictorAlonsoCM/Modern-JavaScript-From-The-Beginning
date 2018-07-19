a42cd3817c6de93b8774d48762e49722b633c9a4
Victor Alonso <vacontrerasmeza@gmail.com>
2018-07-18 22:30:43 -0500

    2_2_Console

--- JavaScript/Section-2/app.js
+++ JavaScript/Section-2/app.js
@@ -1,1 +1,21 @@
-alert("Hello World");
+//Log to console
+console.log('Hello');
+console.log(123);
+console.log(true);
+var greeting = "Hello world";
+console.log(greeting);
+console.log([1,2,3,4,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error("This is a error");
+console.clear();
+console.warn("This's a warning");
+console.time("Hello");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+console.timeEnd("Hello");
