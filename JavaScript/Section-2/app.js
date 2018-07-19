a24a4ad6ab6eb51f7e4ac2db4ca7e28ac175534f
Victor Alonso <vacontrerasmeza@gmail.com>
2018-07-18 22:48:30 -0500

    2_3_Variables

--- JavaScript/Section-2/app.js
+++ JavaScript/Section-2/app.js
@@ -1,21 +1,46 @@
-//Log to console
-console.log('Hello');
-console.log(123);
-console.log(true);
-var greeting = "Hello world";
-console.log(greeting);
-console.log([1,2,3,4,5]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
+// var, let, const
 
-console.error("This is a error");
-console.clear();
-console.warn("This's a warning");
-console.time("Hello");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-console.timeEnd("Hello");
+var name = "Victor Alonso";
+console.log(name);
+name = 'Contreras';
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = "Hello";
+console.log(greeting);
+
+// letters, numbers, _, $
+// Can not start with a number
+
+// Multi word vars
+var firstName = "Victor";
+var first_name = "Sara";
+var FirstName = "Tom";
+var firstname = "Lore";
+
+// LET & CONST
+let name2 = "Victor";
+console.log(name2);
+name2 = "Alonso";
+console.log(name2); 
+const name3 = "Victor";
+console.log(name3);
+// Have to assing a value
+//const constante;
+
+//This we can change its values
+const person = {
+    name4: "Victor",
+    age: 23
+}
+person.name4 = "Alonso";
+person.age = 24;
+console.log(person);
+//Here we can also change its values
+//But not the all structure
+const numbers = [1,2,3,4,5];
+console.log(numbers);
+numbers.push(6);
+console.log(numbers);
